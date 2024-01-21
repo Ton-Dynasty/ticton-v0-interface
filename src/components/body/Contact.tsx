491a6c5920f83955d7701ce3fd057f804224c2cf
alan890104 <alan890104@gmail.com>
2024-01-21 18:02:29 +0800

    Refactor Contact component styling

--- src/components/body/Contact.tsx
+++ src/components/body/Contact.tsx
@@ -1,40 +1,40 @@
 const Contact = () => {
-  const communityNumber = () => {
+  const getCommunityNumber = () => {
     const num = Math.floor(Math.random() * 1e6);
     console.log(num);
     return num;
   };
   return (
-    <section className="flex flex-col w-full  justify-center items-center bg-blue-300 rounded-2xl py-10 md:py-20 max-w-screen-md mx-auto px-16 md:px-20 gap-10">
-      <div className=" w-full text-blue-950 text-lg md:text-3xl font-bold font-droid leading md:leading-10 md:text-justify">
+    <section className="mx-auto flex w-full  max-w-screen-md flex-col items-center justify-center gap-10 rounded-2xl bg-blue-300 px-16 py-10 md:px-20 md:py-20">
+      <div className=" leading w-full font-droid text-lg font-bold text-blue-950 md:text-justify md:text-3xl md:leading-10">
         We are a community of dreamers and innovators from around the world who
         are creating a better future and solving the problems of today.
       </div>
-      <div className="text-black text-base md:text-xl font-normal font-dmsans md:leading-7 md:text-justify w-full md:w-2/3 ml-auto">
+      <div className="ml-auto w-full font-dmsans text-base font-normal text-black md:w-2/3 md:text-justify md:text-xl md:leading-7">
         We invent and use the latest technologies that enable the open, free,
         decentralized exchange of value, information and ideas. Technologies
         that are designed to protect the freedom, privacy and rights of all
         people, to make this world a more balanced and self-governing place.
       </div>
-      <div className="flex flex-col items-center md:items-start w-full">
-        <div className="text-blue-900 text-xl md:text-2xl font-normal font-dohyeon leading-10">
+      <div className="flex w-full flex-col items-center md:items-start">
+        <div className="font-dohyeon text-xl font-normal leading-10 text-blue-900 md:text-2xl">
           Total community member
         </div>
-        <div className="flex mt-2 gap-7">
-          <div className="flex text-base md:text-3xl leading-10 gap-1.5">
-            {communityNumber()
+        <div className="mt-2 flex gap-7">
+          <div className="flex gap-1.5 text-base leading-10 md:text-3xl">
+            {getCommunityNumber()
               .toString()
               .padStart(6, "0")
               .split("")
               .map((digit, index) => {
                 let seperator = undefined;
-                if (index === 3) {
+                if (index % 3 === 0 && index !== 0) {
                   seperator = <div className="m-2" />;
                 }
                 return (
                   <>
                     {seperator}
-                    <div className="flex md:h-16 bg-blue-900 text-white px-5 py-2.5 rounded-[20px] items-center justify-center">
+                    <div className="flex items-center justify-center rounded-[20px] bg-blue-900 px-5 py-2.5 text-white md:h-16">
                       {digit}
                     </div>
                   </>

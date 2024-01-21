b3b4e3548e9b9a5efde1c252ecbf2329221661a3
alan890104 <alan890104@gmail.com>
2024-01-21 20:27:34 +0800

    feat: hover animation in feature section

--- src/components/body/Feature.tsx
+++ src/components/body/Feature.tsx
@@ -3,109 +3,130 @@ import ImageLowLatency from "@/assets/feature/low-latency.png";
 import ImageUnrestricted from "@/assets/feature/unrestricted.png";
 import ImageSafeGuard from "@/assets/feature/safe-guard.png";
 import ImageHighTPS from "@/assets/feature/high-tps.png";
+import { motion } from "framer-motion";
 
 const Feature = () => {
   return (
-    <section className="flex flex-col justify-center items-center gap-16 py-10">
-      <div className="text-center text-2xl font-normal font-bungee leading-9 title-gradient bg-clip-text text-transparent">
+    <section className="flex flex-col items-center justify-center gap-16 py-10">
+      <div className="title-gradient bg-clip-text text-center font-bungee text-2xl font-normal leading-9 text-transparent">
         Feature
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 text-white">
+      <div className="grid grid-cols-1 gap-6 text-white md:grid-cols-3">
         {/* Decentralization */}
-        <div className="w-full md:h-96 px-2 pt-8 bg-indigo-400 bg-opacity-10 rounded-2xl flex-col justify-start items-center gap-6 inline-flex">
-          <div className="h-24 md:h-36 flex-col justify-center items-center gap-6 flex">
-            <div className="self-stretch h-24 md:h-36 flex-col justify-center items-center gap-2 flex">
-              <div className="self-stretch h-16 text-center text-slate-300 text-2xl font-medium font-baloo leading-9">
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          className="inline-flex w-full flex-col items-center justify-start gap-6 rounded-2xl bg-indigo-400 bg-opacity-10 px-2 pt-8 md:h-96"
+        >
+          <div className="flex h-24 flex-col items-center justify-center gap-6 md:h-36">
+            <div className="flex h-24 flex-col items-center justify-center gap-2 self-stretch md:h-36">
+              <div className="h-16 self-stretch text-center font-baloo text-2xl font-medium leading-9 text-slate-300">
                 Decentralization
               </div>
-              <div className="w-full h-16 text-center text-slate-300 text-base font-medium font-inter leading-normal md:text-sm">
+              <div className="h-16 w-full text-center font-inter text-base font-medium leading-normal text-slate-300 md:text-sm">
                 Permissionless quotation
               </div>
             </div>
           </div>
-          <div className="w-full mt-4 h-56 relative overflow-hidden">
+          <div className="relative mt-4 h-56 w-full overflow-hidden">
             <img
-              className="w-full h-auto top-0 absolute hover:bg-[url('/src/assets/feature/hover/magic.gif')]"
+              className="absolute top-0 h-auto w-full hover:bg-[url('/src/assets/feature/hover/magic.gif')]"
               src={ImageDecentralization}
             />
           </div>
-        </div>
+        </motion.button>
         {/* Low latency */}
-        <div className="w-full md:h-96 px-2 pt-8 bg-indigo-400 bg-opacity-10 rounded-2xl flex-col justify-start items-center gap-6 inline-flex">
-          <div className="h-24 md:h-36 flex-col justify-center items-center gap-6 flex">
-            <div className="self-stretch h-24 md:h-36 flex-col justify-center items-center gap-2 flex">
-              <div className="self-stretch h-16 text-center text-slate-300 text-2xl font-medium font-baloo leading-9">
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          className="inline-flex w-full flex-col items-center justify-start gap-6 rounded-2xl bg-indigo-400 bg-opacity-10 px-2 pt-8 md:h-96"
+        >
+          <div className="flex h-24 flex-col items-center justify-center gap-6 md:h-36">
+            <div className="flex h-24 flex-col items-center justify-center gap-2 self-stretch md:h-36">
+              <div className="h-16 self-stretch text-center font-baloo text-2xl font-medium leading-9 text-slate-300">
                 Low-latency
               </div>
-              <div className="w-full h-16 text-center text-slate-300 text-base font-medium font-inter leading-normal md:text-sm">
+              <div className="h-16 w-full text-center font-inter text-base font-medium leading-normal text-slate-300 md:text-sm">
                 Reflect the real market price
               </div>
             </div>
           </div>
-          <div className="w-full mt-4 h-56 relative overflow-hidden">
+          <div className="relative mt-4 h-56 w-full overflow-hidden">
             <img
-              className="w-full h-auto top-0 absolute hover:bg-[url('/src/assets/feature/hover/magic.gif')]"
+              className="absolute top-0 h-auto w-full hover:bg-[url('/src/assets/feature/hover/magic.gif')]"
               src={ImageLowLatency}
             />
           </div>
-        </div>
+        </motion.button>
         {/* Unrestricted */}
-        <div className="w-full md:h-96 px-2 pt-8 bg-indigo-400 bg-opacity-10 rounded-2xl flex-col justify-start items-center gap-6 inline-flex">
-          <div className="h-24 md:h-36 flex-col justify-center items-center gap-6 flex">
-            <div className="self-stretch h-24 md:h-36 flex-col justify-center items-center gap-2 flex">
-              <div className="self-stretch h-16 text-center text-slate-300 text-2xl font-medium font-baloo leading-9">
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          className="inline-flex w-full flex-col items-center justify-start gap-6 rounded-2xl bg-indigo-400 bg-opacity-10 px-2 pt-8 md:h-96"
+        >
+          <div className="flex h-24 flex-col items-center justify-center gap-6 md:h-36">
+            <div className="flex h-24 flex-col items-center justify-center gap-2 self-stretch md:h-36">
+              <div className="h-16 self-stretch text-center font-baloo text-2xl font-medium leading-9 text-slate-300">
                 Unrestricted
               </div>
-              <div className="w-full h-16 text-center text-slate-300 text-base font-medium font-inter leading-normal md:text-sm">
+              <div className="h-16 w-full text-center font-inter text-base font-medium leading-normal text-slate-300 md:text-sm">
                 Any third party with no threshold
               </div>
             </div>
           </div>
-          <div className="w-full mt-4 h-56 relative overflow-hidden">
+          <div className="relative mt-4 h-56 w-full overflow-hidden">
             <img
-              className="w-full h-auto top-0 absolute hover:bg-[url('/src/assets/feature/hover/magic.gif')]"
+              className="absolute top-0 h-auto w-full hover:bg-[url('/src/assets/feature/hover/magic.gif')]"
               src={ImageUnrestricted}
             />
           </div>
-        </div>
+        </motion.button>
         {/* Safe guarding */}
-        <div className="md:col-span-3  w-full px-2 pt-8 md:px-0 md:pt-0 md:h-52 bg-indigo-400 bg-opacity-10 rounded-2xl md:flex md:justify-evenly items-center md:gap-6">
-          <div className="h-24 md:h-36 flex-col md:flex-row justify-center items-center gap-6 flex md:order-last">
-            <div className="self-stretch h-24 md:h-36 flex-col justify-center items-center gap-2 flex">
-              <div className="self-stretch h-16 text-center text-slate-300 text-2xl font-medium font-baloo leading-9">
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-full  items-center rounded-2xl bg-indigo-400 bg-opacity-10 px-2 pt-8 md:col-span-3 md:flex md:h-52 md:justify-evenly md:gap-6 md:px-0 md:pt-0"
+        >
+          <div className="flex h-24 flex-col items-center justify-center gap-6 md:order-last md:h-36 md:flex-row">
+            <div className="flex h-24 flex-col items-center justify-center gap-2 self-stretch md:h-36">
+              <div className="h-16 self-stretch text-center font-baloo text-2xl font-medium leading-9 text-slate-300">
                 Stable and Safe-guarding
               </div>
-              <div className="w-full h-16 text-center text-slate-300 text-base font-medium font-inter leading-normal md:flex md:items-center md:justify-center md:h-2 md:text-sm">
+              <div className="h-16 w-full text-center font-inter text-base font-medium leading-normal text-slate-300 md:flex md:h-2 md:items-center md:justify-center md:text-sm">
                 Stablelized price informations reduce risks
               </div>
             </div>
           </div>
-          <div className="w-full mt-4 h-56 relative overflow-hidden md:h-44 md:mt-0 md:w-44 md:order-first md:flex md:justify-center md:items-center">
+          <div className="relative mt-4 h-56 w-full overflow-hidden md:order-first md:mt-0 md:flex md:h-44 md:w-44 md:items-center md:justify-center">
             <img
-              className="w-full h-auto top-0 absolute md:top-auto md:block hover:bg-[url('/src/assets/feature/hover/magic.gif')]"
+              className="absolute top-0 h-auto w-full hover:bg-[url('/src/assets/feature/hover/magic.gif')] md:top-auto md:block"
               src={ImageSafeGuard}
             />
           </div>
-        </div>
+        </motion.button>
         {/* High TPS */}
-        <div className="md:col-span-3 px-2 pt-8 md:px-0 md:pt-0 w-full md:h-52 bg-indigo-400 bg-opacity-10 rounded-2xl md:flex md:justify-evenly items-center md:gap-6">
-          <div className="h-24 md:h-36 flex-col md:flex-row justify-center items-center gap-6 flex md:order-first">
-            <div className="self-stretch h-24 md:h-36 flex-col justify-center items-center gap-2 flex">
-              <div className="self-stretch h-16 text-center text-slate-300 text-2xl font-medium font-baloo leading-9">
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-full items-center rounded-2xl bg-indigo-400 bg-opacity-10 px-2 pt-8 md:col-span-3 md:flex md:h-52 md:justify-evenly md:gap-6 md:px-0 md:pt-0"
+        >
+          <div className="flex h-24 flex-col items-center justify-center gap-6 md:order-first md:h-36 md:flex-row">
+            <div className="flex h-24 flex-col items-center justify-center gap-2 self-stretch md:h-36">
+              <div className="h-16 self-stretch text-center font-baloo text-2xl font-medium leading-9 text-slate-300">
                 High TPS in TON
               </div>
-              <div className="w-full h-16 text-center text-slate-300 text-base font-normal font-inter leading-normal md:flex md:items-center md:justify-center md:h-2">
+              <div className="h-16 w-full text-center font-inter text-base font-normal leading-normal text-slate-300 md:flex md:h-2 md:items-center md:justify-center">
                 TON's high TPS boosts the quotation
               </div>
             </div>
           </div>
-          <div className="w-full mt-4 h-56 relative overflow-hidden md:h-44 md:mt-0 md:w-44 md:order-last md:flex md:justify-center md:items-center">
+          <div className="relative mt-4 h-56 w-full overflow-hidden md:order-last md:mt-0 md:flex md:h-44 md:w-44 md:items-center md:justify-center">
             <img
-              className="w-full h-auto top-0 absolute md:top-auto md:block hover:bg-[url('/src/assets/feature/hover/magic.gif')]"
+              className="absolute top-0 h-auto w-full hover:bg-[url('/src/assets/feature/hover/magic.gif')] md:top-auto md:block"
               src={ImageHighTPS}
             />
           </div>
-        </div>
+        </motion.button>
       </div>
     </section>
   );

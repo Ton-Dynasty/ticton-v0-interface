c587031ca8570b193ee3a200c77a0b8004276cbe
alan890104 <alan890104@gmail.com>
2024-01-24 00:20:35 +0800

    fix: introduction animation bug

--- src/components/body/Introduction.tsx
+++ src/components/body/Introduction.tsx
@@ -1,5 +1,5 @@
 import { Banknote, UsersRound, Coins } from "lucide-react";
-import { fadeIn } from "../animation/fade";
+// import { fadeIn } from "../animation/fade";
 import { motion } from "framer-motion";
 import vibrant3D from "@/assets/vibrant-3D.png";
 
@@ -22,28 +22,44 @@ const Introduction = () => {
     },
   ];
   return (
-    <motion.section className="flex w-full flex-col items-center justify-center md:h-screen">
+    <motion.section className="bg-star flex w-full flex-col items-center justify-center md:min-h-screen">
       <div className="flex w-full flex-col items-center justify-center gap-9">
         <motion.div
-          initial="hidden"
-          variants={fadeIn("up", 0.3)}
-          whileInView={"show"}
+          initial={{ opacity: 0 }}
+          whileInView={{
+            opacity: 1,
+            transition: {
+              type: "tween",
+              ease: "easeIn",
+              duration: 1,
+              delay: 0.1,
+            },
+          }}
           className="title-gradient bg-clip-text text-center font-bungee text-2xl font-normal leading-9 text-transparent"
         >
           WHAT IS TICTON?
         </motion.div>
         <motion.span
-          initial="hidden"
-          variants={fadeIn("up", 0.3)}
-          whileInView={"show"}
+          initial={{ opacity: 0 }}
+          whileInView={{
+            opacity: 1,
+            transition: {
+              type: "tween",
+              ease: "easeIn",
+              duration: 1,
+              delay: 0.3,
+            },
+          }}
           className="w-full text-center text-xl font-bold leading-loose text-slate-300 md:text-5xl"
         >
           Pure Decentralized Oracle on Ton
         </motion.span>
         <motion.span
-          initial="hidden"
-          variants={fadeIn("up", 0.3)}
-          whileInView={"show"}
+          initial={{ opacity: 0 }}
+          whileInView={{
+            opacity: 1,
+            transition: { ease: "easeIn", duration: 1, delay: 0.3 },
+          }}
           className="w-full text-pretty px-6 font-duru text-base font-normal leading-normal text-slate-300 md:text-center md:leading-loose"
         >
           Tic Ton oracle stands as the premier fully decentralized oracle on the
@@ -55,13 +71,18 @@ const Introduction = () => {
         </motion.span>
         <div className="m-4 flex flex-col gap-2 text-white md:flex-row md:gap-4">
           <motion.div
-            initial="hidden"
-            variants={fadeIn("left", 0.3)}
-            whileInView={"show"}
+            initial={{ opacity: 0 }}
+            whileInView={{
+              opacity: 1,
+              transition: { ease: "easeIn", duration: 1, delay: 0.3 },
+            }}
             className="mx-auto flex w-auto flex-col items-center  justify-between md:h-auto md:w-1/2"
           >
             {data.map(({ title, value, icon }) => (
-              <div className="flex h-32 w-full flex-row items-center justify-start gap-4 md:ml-20 md:h-full">
+              <div
+                className="flex h-32 w-full flex-row items-center justify-start gap-4 md:ml-20 md:h-full"
+                key={`intro-${title}`}
+              >
                 <div className="flex h-14 w-14 items-center justify-center rounded-xl bg-zinc-400 bg-opacity-10 md:mx-0">
                   {icon}
                 </div>
@@ -75,9 +96,11 @@ const Introduction = () => {
             ))}
           </motion.div>
           <motion.div
-            initial="hidden"
-            variants={fadeIn("right", 0.3)}
-            whileInView={"show"}
+            initial={{ opacity: 0 }}
+            whileInView={{
+              opacity: 1,
+              transition: { ease: "easeIn", duration: 1, delay: 0.3 },
+            }}
             className="order-first flex justify-center md:order-none md:w-1/2 md:justify-end"
           >
             <img src={vibrant3D} alt="" />
